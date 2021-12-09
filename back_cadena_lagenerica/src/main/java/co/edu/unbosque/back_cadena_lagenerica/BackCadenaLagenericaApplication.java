5d9a6eb1758a85dc45c02f3e49c013ed561f98b7
Hans_Escobar <escobar.hans@gmail.com>
2021-12-08 19:28:16 -0500

    starting proyect

---
+++ back_cadena_lagenerica/src/main/java/co/edu/unbosque/back_cadena_lagenerica/BackCadenaLagenericaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.back_cadena_lagenerica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCadenaLagenericaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCadenaLagenericaApplication.class, args);
+	}
+
+}
---
+++ back_cadena_lagenerica/src/test/java/co/edu/unbosque/back_cadena_lagenerica/BackCadenaLagenericaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.back_cadena_lagenerica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackCadenaLagenericaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
